82f84d04ac6df801a92c3901d07836ed10c43831
Jazmin Villagra <jazvillagra.jv@gmail.com>
2020-11-05 22:29:38 -0300

    Initial commit

---
+++ src/main/java/com/github/jazvillagra/redhospitalaria/RedHospitalariaApplication.java
@@ -0,0 +1,13 @@
+package com.github.jazvillagra.redhospitalaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedHospitalariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedHospitalariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/jazvillagra/redhospitalaria/RedHospitalariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jazvillagra.redhospitalaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedHospitalariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
