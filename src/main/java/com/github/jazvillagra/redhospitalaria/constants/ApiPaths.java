1c7dc901f154a4341f90ef0648ce02c7081de95f
Jazmin Villagra <jazvillagra.jv@gmail.com>
2020-11-06 01:34:31 -0300

    Agrega endpoints para guardar y obtener datos de servicio

--- src/main/java/com/github/jazvillagra/redhospitalaria/constants/ApiPaths.java
+++ src/main/java/com/github/jazvillagra/redhospitalaria/constants/ApiPaths.java
@@ -10,4 +10,11 @@ public class ApiPaths {
     public static final String BASE_HOSPITAL = BASE + "/hospital";
     public static final String HOSPITAL_SAVE = BASE_HOSPITAL + "/save";
     public static final String HOSPITAL_BY_COD_HOSPITAL = BASE_HOSPITAL + "/{codHospital}";
+
+    /**
+     * Servicio URLs
+     */
+    public static final String BASE_SERVICIO = BASE + "/servicio";
+    public static final String SERVICIO_SAVE = BASE_SERVICIO + "/save";
+    public static final String SERVICIO_BY_COD_SERVICIO = BASE_SERVICIO + "/{codServicio}";
 }
--- src/main/java/com/github/jazvillagra/redhospitalaria/web/controller/ApiController.java
+++ src/main/java/com/github/jazvillagra/redhospitalaria/web/controller/ApiController.java
@@ -2,12 +2,16 @@ package com.github.jazvillagra.redhospitalaria.web.controller;
 
 import com.github.jazvillagra.redhospitalaria.constants.ApiPaths;
 import com.github.jazvillagra.redhospitalaria.dto.HospitalDTO;
+import com.github.jazvillagra.redhospitalaria.dto.ServicioDTO;
 import com.github.jazvillagra.redhospitalaria.service.HospitalService;
+import com.github.jazvillagra.redhospitalaria.service.ServicioService;
 import com.github.jazvillagra.redhospitalaria.web.response.ListResponseDTO;
 import com.github.jazvillagra.redhospitalaria.web.response.ObjectResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author jazvillagra
@@ -19,6 +23,8 @@ public class ApiController {
     @Autowired
     private HospitalService hospitalService;
 
+    @Autowired
+    private ServicioService servicioService;
     /**
      * Hospital Services
      */
@@ -35,5 +41,24 @@ public class ApiController {
     @GetMapping(ApiPaths.HOSPITAL_BY_COD_HOSPITAL)
     public ResponseEntity<ObjectResponseDTO<HospitalDTO>> getHospitalByCodHospital(@PathVariable String codHospital){
         return ResponseEntity.ok(ObjectResponseDTO.success(hospitalService.getByCodHospital(codHospital)));
+    }
+
+    /**
+     * Servicio
+     * @return
+     */
+    @GetMapping(ApiPaths.BASE_SERVICIO)
+    public ResponseEntity<ListResponseDTO<ServicioDTO>> getAllServicios(){
+        return ResponseEntity.ok(ListResponseDTO.success(servicioService.getAllServicios()));
+    }
+
+    @PostMapping(ApiPaths.SERVICIO_SAVE)
+    public ResponseEntity<ObjectResponseDTO<ServicioDTO>> saveService(@RequestBody ServicioDTO servicioDTO){
+        return ResponseEntity.ok(ObjectResponseDTO.success(servicioService.save(servicioDTO)));
+    }
+
+    @GetMapping(ApiPaths.SERVICIO_BY_COD_SERVICIO)
+    public ResponseEntity<ObjectResponseDTO<ServicioDTO>> getServicioByCodServicio(@PathVariable String codServicio) {
+        return ResponseEntity.ok(ObjectResponseDTO.success(servicioService.getByCodServicio(codServicio)));
     }
 }
